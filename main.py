c160d1d20ab8483f430f67af406402433cfd4b7f
Thorin, the Bearded <johnjonsteyn@tutanota.com>
2023-05-25 17:40:16 +0200

    Hide + and - buttons
    + Hide decrease button when timer starts.
    + Hide increase button when timer starts.
    + Show decrease button when timer stops.
    + Show increase button when timer stops.
    - Remove comments.

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.startButton.clicked.connect(self.startTimer)
         self.resetButton.clicked.connect(self.resetTimer)
         self.increaseButton.clicked.connect(self.increaseTime)
-        self.decreaseButton.clicked.connect(self.decreaseTime)  # Connect decreaseButton
+        self.decreaseButton.clicked.connect(self.decreaseTime)
         self.workTimer = QTimer()
         self.workTimer.setInterval(1000)
         self.workTimer.timeout.connect(self.updateWork)
@@ -30,17 +30,21 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     def startTimer(self):
         self.startWork()
         self.startButton.setEnabled(False)
-        self.countdownStopped = False  # Countdown started, set countdownStopped to False
+        self.countdownStopped = False
+        self.decreaseButton.hide()
+        self.increaseButton.hide()
 
     def resetTimer(self):
         self._setDefaults()
         mins, secs = divmod(self.timeSet, 60)
         self.timeLCD.display(f"{mins:02d}:{secs:02d}")
+        self.decreaseButton.show()
+        self.increaseButton.show()
 
     def startWork(self):
         self.modeLabel.setText("Work")
         self.modeLabel.setStyleSheet("color: red")
-        self.timeRemainingSec = self.countdownSec  # Use the selected time as the starting time
+        self.timeRemainingSec = self.countdownSec
         mins, secs = divmod(self.timeRemainingSec, 60)
         remainingText = f"{mins:02d}:{secs:02d}"
         self.timeLCD.display(remainingText)
@@ -76,7 +80,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             self.startWork()
 
     def updateTimeDisplay(self):
-        if not self.countdownStopped:  # Only update time if countdown is not stopped
+        if not self.countdownStopped:
             self.timeRemainingSec -= 1
         mins, secs = divmod(self.timeRemainingSec, 60)
         mins = f"{mins:02d}"
@@ -86,21 +90,21 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
     def decreaseTime(self):
         if self.countdownStopped:
-            self.timeSet -= 60  # Decrease time by a minute
-            if self.timeSet < 60:  # Check if timeSet is less than 1 minute
-                self.timeSet = 60  # Set timeSet to 1 minute
+            self.timeSet -= 60
+            if self.timeSet < 60:
+                self.timeSet = 60
             mins, secs = divmod(self.timeSet, 60)
             self.countdownSec = self.timeSet
-            self.timeLCD.display(f"{mins:02d}:{secs:02d}")  # Update time display
+            self.timeLCD.display(f"{mins:02d}:{secs:02d}")
 
     def increaseTime(self):
         if self.countdownStopped:
-            self.timeSet += 60  # Increase time by a minute
-            if self.timeSet > 120 * 60:  # Check if timeSet exceeds 120 minutes
-                self.timeSet = 120 * 60  # Set timeSet to 120 minutes
+            self.timeSet += 60
+            if self.timeSet > 120 * 60:
+                self.timeSet = 120 * 60
             mins, secs = divmod(self.timeSet, 60)
             self.countdownSec = self.timeSet
-            self.timeLCD.display(f"{mins:02d}:{secs:02d}")  # Update time display
+            self.timeLCD.display(f"{mins:02d}:{secs:02d}")
 
     def _setDefaults(self):
         self.workSessions = 0
@@ -115,8 +119,8 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         secs = f"{0:02d}"
         remainingText = mins + ":" + secs
         self.timeLCD.display(remainingText)
-        self.countdownStopped = True  # Set countdownStopped to True on reset
-        self.countdownSec = WORK_MIN * 60  # Reset countdownSec to initial work duration
+        self.countdownStopped = True
+        self.countdownSec = WORK_MIN * 60
 
 def run():
     ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(APP_ID)
